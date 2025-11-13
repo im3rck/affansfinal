c1ac86e455a8aa6a8597b995a66711bd8d8359ef
im3rck <71458376+im3rck@users.noreply.github.com>
2025-11-13 15:57:45 +0530

    Delete testgemini.py

--- testgemini.py
+++
@@ -1,5 +0,0 @@
-import google.generativeai as genai
-genai.configure(api_key="AIzaSyAKrO_ucEVGefsHBn7wcP_IMSBS3yqmA9E" )
-for model in genai.list_models():
-    if 'generateContent' in model.supported_generation_methods:
-        print(model.name)
